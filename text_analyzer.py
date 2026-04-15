3a1f31b24f05e3560659ef84e2cb80d2f43d5f7d
ROSE A. ODHIAMBO <odhiamborose466@gmail.com>
2026-04-15 22:36:39 +0300

    Assignment starter: assignment/week-1-day-1-slug

---
+++ text_analyzer.py
@@ -0,0 +1,13 @@
+# Text Analyzer
+
+# TODO: Accept input from the user
+input_text = ''  # Replace this with user input
+
+# TODO: Split the input text into words
+words = []  # How will you split the string?
+
+# TODO: Count the frequency of each word
+word_count = {}  # How will you store the counts?
+
+# TODO: Print the word count dictionary
+print(word_count)  # What format do you want to output?
